d0fb9a74dab561d282971a762bf68471a37d1b83
Hendrik Liebau <mail@hendrik-liebau.de>
2026-02-24 23:04:20 +0100

    Integrate review feedback

--- packages/next/src/server/app-render/app-render.tsx
+++ packages/next/src/server/app-render/app-render.tsx
@@ -830,7 +830,10 @@ async function generateStagedDynamicFlightRenderResult(
       return dynamicStream
     },
     () => {
-      finishStaleTimeTracking(staleTimeIterable)
+      // This is a separate task that doesn't advance a stage. It forces
+      // draining the microtask queue so that the stale time iterable is closed
+      // before we advance to the dynamic stage.
+      void finishStaleTimeTracking(staleTimeIterable)
     },
     () => {
       stageController.advanceStage(RenderStage.Dynamic)
@@ -3579,7 +3582,7 @@ async function countStaticStageBytes(
   let byteLength = 0
   const reader = stream.getReader()
 
-  stageController.waitForStage(RenderStage.EarlyRuntime).then(() => {
+  stageController.onStage(RenderStage.EarlyRuntime, () => {
     reader.cancel()
   })
 
